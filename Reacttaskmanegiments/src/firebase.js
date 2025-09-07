9accf133fa25581388a7aef757ced488fbc75171
hirorogo <hiro114514@proton.me>
2025-09-07 22:27:20 +0900

    firebaseに付いて学び初期設定を済ませた

---
+++ Reacttaskmanegiments/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDBMQrw6105Cyq7cJ3n66-ZlLQPeQf7dcQ",
+  authDomain: "react-task-78b00.firebaseapp.com",
+  projectId: "react-task-78b00",
+  storageBucket: "react-task-78b00.firebasestorage.app",
+  messagingSenderId: "721160572505",
+  appId: "1:721160572505:web:f4ec44950d080e1d83eb48"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
